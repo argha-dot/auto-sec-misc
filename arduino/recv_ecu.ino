919001b1f66473ec5bf68e8c24c0d14dae1254fd
imhim <47056150+imhim@users.noreply.github.com>
2022-06-24 11:12:16 +0530

    recv changes

--- arduino/recv_ecu.ino
+++ arduino/recv_ecu.ino
@@ -1,35 +1,43 @@
 #include <SPI.h>
 #include "mcp2515_can.h"
 
-const int SPI_CS_PIN = 10;
-const int CAN_INT_PIN = 2;
+const int spiCSPin = 10;
+const int ledPin = 2;
+boolean ledON = 1;
 
-long unsigned int msgId;
-unsigned char msgLen = 0;
-unsigned char msgBuf[8];
-char msgString[128];
-
-mcp2515_can CAN(SPI_CS_PIN);
+mcp2515_can CAN(spiCSPin);
 
 void setup() {
-    Serial.begin(115200);
+  Serial.begin(115200);
+  pinMode(ledPin,OUTPUT);
 
   while (CAN_OK != CAN.begin(CAN_500KBPS, MCP_16MHz)) {
     Serial.println("CAN BUS Init Failed");
     delay(100);
   }
+  
   Serial.println("CAN BUS  Init OK!");
-  CAN.setMode(MODE_LISTENONLY);
+  CAN.setMode(MODE_NORMAL);
 }
 
 void loop() {
-  if (!digitalRead(CAN_INT_PIN)) {
-    CAN.readMsgBufID(&msgId, &msgLen, msgBuf);
+    unsigned char len = 0;
+    unsigned char buf[8];
 
-    sprintf(msgString, "ID: 0x%.8lx;", msgId);
-    Serial.print(msgString);
+    if (CAN_MSGAVAIL == CAN.checkReceive()) {
+      CAN.readMsgBuf(&len, buf);
 
-    sprintf(msgString, "MSG: 0x%.8lx;", msgBuf);
-    Serial.println(msgString);
-  }
+      unsigned long canId = CAN.getCanId();
+
+      Serial.print("ID: 0x");
+      Serial.print(canId, HEX);
+
+      Serial.print(" MSG: ");
+      
+      for (int j = 0; j < len; j++) {
+        Serial.print(buf[j], HEX);
+      }
+
+      Serial.println();
+    }
 }
