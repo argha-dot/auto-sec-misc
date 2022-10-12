e4dcafae9eb0d3b8df33eb48696f7c3a68dfd6b8
argha-dot <63609014+argha-dot@users.noreply.github.com>
2022-10-12 17:01:36 +0530

    Update with the new API

--- arduino/send_test.ino
+++ arduino/send_test.ino
@@ -1,38 +1,35 @@
 #include <SPI.h>
 #include "mcp2515_can.h"
 
-const int spiCSPin = 10;
-
-mcp2515_can CAN(spiCSPin);
+mcp2515_can CAN(10);
 
 void setup() {
-    Serial.begin(115200);
-
-    while (CAN_OK != CAN.begin(CAN_500KBPS)) {
-        Serial.println("CAN BUS Init Failed");
-        delay(100);
-    }
-    Serial.println("CAN BUS  Init OK!");
+  // put your setup code here, to run once:
+   Serial.begin(115200);
+   while(!Serial){};
+   while (CAN_OK != CAN.begin(CAN_500KBPS)) {
+      Serial.println("CAN init fail, retry...");
+      delay(100);
+   }
+   Serial.println("CAN init ok!");
 }
 
+unsigned char stmp[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 
 void loop() {
-    unsigned char len = 0;
-    unsigned char buf[8];
+  // put your main code here, to run repeatedly:
+  stmp[7] = stmp[7] + 1;
+   if (stmp[7] == 100) {
+      stmp[7] = 0;
+      stmp[6] = stmp[6] + 1;
+      if (stmp[6] == 100) {
+         stmp[6] = 0;
+         stmp[5] = stmp[5] + 1;
+      }
+   }
+   
+   CAN.sendMsgBuf(0x00, 0, 0, 8, stmp, true);
+   delay(100); // send data per 100ms
+   Serial.println("CAN BUS sendMsgBuf ok!");
 
-    if(CAN_MSGAVAIL == CAN.checkReceive()) {
-        CAN.readMsgBuf(&len, buf);
-
-        unsigned long canId = CAN.getCanId();
-
-        Serial.println("-----------------------------");
-        Serial.print("Data from ID: 0x");
-        Serial.println(canId, HEX);
-
-        for(int i = 0; i<len; i++) {
-          Serial.print(buf[i]);
-          Serial.print("\t");
-          Serial.println();
-        }
-    }
 }
